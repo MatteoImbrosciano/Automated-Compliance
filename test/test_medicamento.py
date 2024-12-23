d39adfaf24063cfd88c9759982bcb35ac7fe265f
Matteo <1000014829@studium.unict.it>
2024-12-23 21:55:43 +0100

    #25, implementati test unitari per la classe medicamento

--- test/test_medicamento.py
+++ test/test_medicamento.py
@@ -7,13 +7,3 @@ def test_medicamento_inizializzazione():
     assert medicamento.cantidad == 100
     assert medicamento.precio == 4.50
     assert medicamento.unitad == "mg"
-
-def test_medicamento_valori_non_validi():
-    with pytest.raises(ValueError):
-        Medicamento("", 100, 4.50, "mg")
-    with pytest.raises(ValueError):
-        Medicamento("Aspirina", -10, 4.50, "mg")
-    with pytest.raises(ValueError):
-        Medicamento("Aspirina", 100, -4.50, "mg")
-    with pytest.raises(ValueError):
-        Medicamento("Aspirina", 100, 4.50, "")
